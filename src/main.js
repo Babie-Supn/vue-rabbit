939c953623deedda32860fd568c09cc12a9a012a
Barbie-Supn <3141730662@qq.com>
2023-11-24 18:48:27 +0800

    axios安装及基础封装

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+//测试接口函数
+import { getCategory } from "@/apis/testAPI";
 
-const app = createApp(App)
+getCategory().then((res) => {
+  console.log(res);
+});
 
-app.use(createPinia())
-app.use(router)
+import App from "./App.vue";
+import router from "./router";
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
