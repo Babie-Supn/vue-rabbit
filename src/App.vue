2f47fcce5a87f4fd8aea573ef79784820f356ee5
Barbie-Supn <3141730662@qq.com>
2023-11-24 18:30:16 +0800

    定制主题色

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 
 <template>
  <el-button type="primary">Primary</el-button>
+ <el-button type="danger">danger</el-button>
+ <el-button type="success">success</el-button>
+ <el-button type="warning">warning</el-button>
+
+
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,25 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        //1.配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
     }),
   ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //2.自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+        @use "@/styles/element/index.scss" as *;
+        `,
+      },
     },
   },
 });
