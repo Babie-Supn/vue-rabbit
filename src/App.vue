f6e6e23633c2a9cff6b95bed6877421a94226e8d
Barbie-Supn <3141730662@qq.com>
2023-11-25 11:52:41 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@
 <template>
   <!-- 一级路由出口组件 -->
   <RouterView />
+  <div class="test">
+    test textColor
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test{
+  color: $xtxColor;
+  background-color: #F3A1A9;
+}
 </style>
