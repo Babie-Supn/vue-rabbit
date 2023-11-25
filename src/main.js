3280d6ccb1df79f9c034794ad2d1b833cd259436
Barbie-Supn <3141730662@qq.com>
2023-11-25 11:37:29 +0800

    引入初始化样式

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+//引入初始化样式
+import "@/styles/common.scss";
 
 //测试接口函数
 import { getCategory } from "@/apis/testAPI";
-
-getCategory().then((res) => {
-  console.log(res);
-});
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+
+getCategory().then((res) => {
+  console.log(res);
+});
 
 app.use(createPinia());
 app.use(router);
