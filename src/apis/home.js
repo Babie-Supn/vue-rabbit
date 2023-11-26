6a4497bf3bd5f1a348f0bef544b22944fb6710f9
Barbie-Supn <3141730662@qq.com>
2023-11-26 20:38:44 +0800

    产品列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -21,3 +21,13 @@ export const getHotAPI = () => {
     url: "/home/hot",
   });
 };
+
+/**
+ * 获取所有商品模块
+ */
+
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: "/home/goods",
+  });
+};
