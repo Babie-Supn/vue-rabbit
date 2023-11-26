1e5d7c1b7bdbd6f781b2aac8d78510d03759f710
Barbie-Supn <3141730662@qq.com>
2023-11-26 17:00:36 +0800

    面板组件封装

--- src/views/Home/components/HomeNew.vue
+++ src/views/Home/components/HomeNew.vue
@@ -1,3 +1,12 @@
+<script setup>
+import HomePanel from './HomePanel.vue';
+</script>
+
+
 <template>
   <h1>我是新鲜好物</h1>
 </template>
+
+<style scoped>
+
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,12 @@
     <HomeBanner/>
   </div>
   <HomeNew/>
+  <!-- 测试面板组件 -->
+  <HomePanel title="新鲜好物" sub-title="推荐">
+    <div >
+      我是新鲜好物的插槽内容
+    </div>
+  </HomePanel>
   <HomeHot />
   <HomeProduct />
 
@@ -16,4 +22,5 @@ import HomeCategory from "@/views/Home/components/HomeCategory.vue"
 import HomeHot from "@/views/Home/components/HomeHot.vue"
 import HomeNew from "@/views/Home/components/HomeNew.vue"
 import HomeProduct from "@/views/Home/components/HomeProduct.vue"
+import HomePanel from "./components/HomePanel.vue"
 </script>
