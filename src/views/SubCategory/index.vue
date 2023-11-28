b461c6434ca60b88b4f05ee68603ccc0b1afeaef
Barbie-Supn <3141730662@qq.com>
2023-11-28 20:22:09 +0800

    瀑布加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 import { useRoute } from "vue-router"
 import GoodsItem from '../Home/components/GoodsItem.vue';
 
+
 //获取面包屑导航数据
 const categoryData = ref([])
 const route =useRoute()
@@ -11,6 +12,8 @@ const getCategoryData = async () => {
   const res = await getCategoryFilterAPI(route.params.id)
   categoryData.value = res.result
 }
+
+
 
 //获取基础列表数据渲染
 const goodsList = ref([])
@@ -30,6 +33,26 @@ onMounted(() => {
   getGoodsList()
 })
 
+//tab切换回调
+
+const tabChange = () => {
+  requestData.value.page=1
+  getGoodsList()
+}
+
+//底部加载load
+const disabled = ref(false)
+const load = async () => {
+  //获取下一页数据
+  requestData.value.page++
+  const res = await getSubCategoryAPI(requestData.value)
+  goodsList.value = [...goodsList.value, ...res.result.items]
+  //加载完毕 停止监听
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
+
 </script>
 
 <template>
@@ -44,14 +67,14 @@ onMounted(() => {
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="requestData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
-         <GoodsItem v-for="goods in goodsList" :key="goods.id" :goods="goods"/>
+         <GoodsItem v-for="goods in goodsList" :key="goods.id" :goods="goods" />
       </div>
     </div>
   </div>
