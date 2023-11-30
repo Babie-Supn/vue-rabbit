9e6812ce51b51e214bab9fc0d57bf3617c4fa559
Barbie-Supn <3141730662@qq.com>
2023-11-30 14:45:07 +0800

    表单基础校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,26 @@
 <script setup>
+import {ref} from "vue"
+
+
+//表单校验（账户名+密码）
+
+//1.准备表单对象
+const form = ref({
+  account: '',
+  password:''
+})
+
+//2.准备规则对象
+const rules = {
+  account: [
+    {required:true,message: '用户名不能为空',trigger:'blur'}
+  ],
+  password: [
+    { required: true, message: '密码不能为空', trigger: 'blur' },
+    { min: 6, max: 14, message: '密码需为6-14个字符', trigger: 'blur' }
+  ]
+}
+
 
 </script>
 
@@ -24,13 +46,17 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px"
+            <el-form 
+            label-position="right" 
+            label-width="60px" 
+            :model="form"
+            :rules="rules"
               status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account"/>
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox  size="large">
