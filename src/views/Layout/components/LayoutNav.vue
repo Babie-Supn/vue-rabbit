9e1c4f80b23f5b4b8741cf93825817263411b517
Barbie-Supn <3141730662@qq.com>
2023-12-01 08:37:52 +0800

    使用Token适配登录显示

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,12 +1,15 @@
 <script setup>
+import { useUserStore } from "@/stores/user"
+
+const userStore = useUserStore()
 
 </script>
 <template>
   <nav class="app-topnav">
     <div class="container">
       <ul>
-        <template v-if="false">
-          <li><a>Barbie-Supn</a></li>
+        <template v-if="userStore.userInfo.token">
+          <li><a><i></i>{{ userStore.userInfo.nickname }}</a></li>
           <li>
           <el-popconfirm title="确认退出吗？" confirm-button-text="确认" cancel-button-text="取消">
             <template #reference>
